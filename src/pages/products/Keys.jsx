fb458a3a80fcd380697efa012795a5965cee0a4f
Majogobla <marcosjgodoyb@gmail.com>
2023-06-18 12:32:44 -0600

    keys debug

--- src/pages/products/Keys.jsx
+++ src/pages/products/Keys.jsx
@@ -87,7 +87,7 @@ function Keys()
   {
     e.preventDefault();
 
-    // console.log(e.target[0].value.trim());
+    console.log(e.target[0].value.trim());
 
     if(e.target[0].value.trim().length > 3)
     {
@@ -108,7 +108,7 @@ function Keys()
                 <svg aria-hidden="true" className="w-5 h-5 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
             </div>
 
-            <input type="search" id="default-search" className="block w-full p-4 pl-10 text-xl text-gray-900 border-2 border-[#fb5910] rounded-lg bg-transparent focus:outline-none" placeholder="Buscar Gift Cards, Games, Software..." name="search"/>
+            <input type="text" id="default-search" className="block w-full p-4 pl-10 text-xl text-gray-900 border-2 border-[#fb5910] rounded-lg bg-transparent focus:outline-none" placeholder="Buscar Gift Cards, Games, Software..." name="search"/>
 
             <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-[#fb5910] hover:bg-[#AD3A05] focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-xl px-4 py-2 transition-colors font-black">Buscar</button>
           </div>
